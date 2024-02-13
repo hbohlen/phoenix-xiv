47715bfd3acf5e55accb137a8ee94f505980ceda
hbohlen <bohlenhayden@gmail.com>
2024-02-13 11:11:32 -0600

    Update dependencies and Prisma schema

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,8 +3,13 @@
 import NextAuth, { Session, User } from "next-auth";
 import { NextApiRequest, NextApiResponse } from "next";
 import DiscordProvider from "next-auth/providers/discord";
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID as string,
@@ -16,19 +21,4 @@ export default NextAuth({
       },
     }),
   ],
-  callbacks: {
-    session: ({ session, token }) => {
-      session.user.id = token.id as string;
-      console.log("Session Callback", { session, token });
-      return session;
-    },
-    jwt: ({ token, user }) => {
-      if (user) {
-        token.id = user.id;
-      }
-
-      console.log("JWT Callback", { token, user });
-      return token;
-    },
-  },
 });
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,7 +9,6 @@ async function main() {
     create: {
       email: "alice@prisma.io",
       name: "Alice",
-      password: "password123",
     },
   });
 
