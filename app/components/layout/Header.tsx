68030dd37abe40af1a724f77a1a6e3236962219e
hbohlen <bohlenhayden@gmail.com>
2024-02-16 15:59:12 -0600

    Add @emotion/react, @emotion/styled, and @mui/material dependencies, and update Header component

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,27 +1,18 @@
+// layout/Header.tsx
+
 import { useSession } from "next-auth/react";
+import { AppBar, Toolbar, Typography, Avatar } from "@mui/material";
 
 const Header: React.FC = () => {
   const { data: session } = useSession();
 
   return (
-    <header
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "10px",
-        borderBottom: "1px solid #ccc",
-      }}
-    >
-      <h1>Phoenix.xiv</h1>
-      {session && (
-        <img
-          src={session.user?.image as string}
-          alt="User"
-          style={{ borderRadius: "50%", height: "40px" }}
-        />
-      )}
-    </header>
+    <AppBar position="static">
+      <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
+        <Typography variant="h6">Phoenix.xiv</Typography>
+        {session && <Avatar src={session.user?.image as string} alt="User" />}
+      </Toolbar>
+    </AppBar>
   );
 };
 
